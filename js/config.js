aeb364dcefdd613918a08e9731d62812ee5e0f3b
Mihishkaa Sinha <72974069+mihishkaasinha@users.noreply.github.com>
2021-05-09 18:49:38 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 var appConfig = {
  apiKey: "AIzaSyCoY-g6OwtPlYO8xzHM3uX4qRxFUNoMWtI",
   authDomain: "not-alone-91a5d.firebaseapp.com",
-  databaseURL: "https://not-alone-91a5d-default-rtdb.firebaseio.com",
+  databaseURL: "https://not-alone-91a5d-default-rtdb.firebaseio.com/",
   projectId: "not-alone-91a5d",
   storageBucket: "not-alone-91a5d.appspot.com",
   messagingSenderId: "859464294287",
