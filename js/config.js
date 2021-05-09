a39276d5f004b265e5be25811a0fd9da0dc6fc3f
Mihishkaa Sinha <72974069+mihishkaasinha@users.noreply.github.com>
2021-05-09 18:38:15 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 // @author: Mihishkaa Sinha
 // configuration details for firebase real time database
 var appConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCoY-g6OwtPlYO8xzHM3uX4qRxFUNoMWtI",
+  authDomain: "not-alone-91a5d.firebaseapp.com",
+  databaseURL: "https://not-alone-91a5d-default-rtdb.firebaseio.com",
+  projectId: "not-alone-91a5d",
+  storageBucket: "not-alone-91a5d.appspot.com",
+  messagingSenderId: "859464294287",
+  appId: "1:859464294287:web:f05d06ab4defb4bf17b5fa"
 }
